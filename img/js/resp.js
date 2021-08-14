6d2ed5600e8d1c1950cde78b2d15607ebe509943
Ayush Negi <86602274+AyushNegi008@users.noreply.github.com>
2021-08-14 23:33:46 +0530

    Update resp.js

--- img/js/resp.js
+++ img/js/resp.js
@@ -1,19 +1,27 @@
 burger = document.querySelector('.burger')
 navbar = document.querySelector('.navbar')
 navList = document.querySelector('.nav-list')
+line = document.querySelector('.line')
+line2 = document.querySelector('.line2')
+
 
 
 burger.addEventListener('click',()=>{
     navList.classList.toggle('v-class-resp');
     navbar.classList.toggle('h-nav-resp');
-    
-
+    burger.classList.toggle('icon');
+    line.classList.toggle('top');
+    line2.classList.toggle('bootom');
 })
+
+
+
 burger2 = document.querySelector('.burger2')
 rightNav = document.querySelector('.rightNav')
 rightNavinput = document.querySelector('.rightNavinput')
 clear = document.querySelector('.clear')
 btnsm= document.querySelector('.btnsm')
+color2 = document.querySelector('.color2')
 
 
 burger2.addEventListener('click',()=>{
@@ -21,6 +29,9 @@ burger2.addEventListener('click',()=>{
     rightNavinput.classList.toggle('vb-class-resp');
     clear.classList.toggle('vb-class-resp');
     btnsm.classList.toggle('vb-class-resp');
+    color2.classList.toggle('no');
+
+
 })
 pocox = document.querySelector('.pocox')
 section = document.querySelector('.section')
